9ca8e4655c1796017c54758cd068f3402de050b9
Vinicius Alexander Moreira Nascimento <vinicius.malex2002@gmail.com>
2025-11-12 20:40:27 -0300

    feat: work with message errors

---
+++ src/Backend/MyRecipeBook.Application/UseCases/User/Register/RegisterUserUsecase.cs
@@ -0,0 +1,37 @@
+﻿using MyRecipeBook.Communication.Request;
+using MyRecipeBook.Communication.Response;
+
+namespace MyRecipeBook.Application.UseCases.User.Register;
+
+public class RegisterUserUsecase
+{
+    public ResponseRegisterUserJson Execute(RequestRegisterUserJson request)
+    {
+        // Validar a Request 
+        Validate(request);
+
+        // Mapear a request em um entidade
+
+        // Cripitografia da senha
+
+        //Salvar no banco de dados
+        return new ResponseRegisterUserJson
+        {
+            Name = request.Name
+        };
+    }
+
+    private void Validate(RequestRegisterUserJson request)
+    {
+        var validator = new RegisterUserValidator();
+
+        var result = validator.Validate(request);
+
+        if (result.IsValid == false)
+        {
+            var errorMessages = result.Errors.Select(e => e.ErrorMessage);
+            throw new Exception();
+        }
+    }
+}
+
---
+++ src/Backend/MyRecipeBook.Application/UseCases/User/Register/RegisterUserValidator.cs
@@ -0,0 +1,17 @@
+﻿using FluentValidation;
+using MyRecipeBook.Communication.Request;
+using MyRecipeBook.Exeptions;
+
+namespace MyRecipeBook.Application.UseCases.User.Register;
+
+public class RegisterUserValidator: AbstractValidator<RequestRegisterUserJson>
+{
+    public RegisterUserValidator()
+    {
+        RuleFor(user => user.Name).NotEmpty().WithMessage(ResourceMessagesExeptions.NAME_EMPY);
+        RuleFor(user => user.Email).NotEmpty().WithMessage(ResourceMessagesExeptions.EMAIL_EMPTY);
+        RuleFor(user => user.Email).NotEmpty().EmailAddress().WithMessage(ResourceMessagesExeptions.EMAIL_INVALID);
+        RuleFor(user => user.Password).NotEmpty().MinimumLength(6).WithMessage(ResourceMessagesExeptions.PASSWORD_EMPTY);
+    }
+}
+
